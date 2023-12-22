4dc33ddbac38c7e0b908acd35c8fb8243182a59c
davlatshoh6275 <davlatshoh6275@gmail.com>
2023-12-22 16:25:36 +0300

    lesson js 3 end

--- main.js
+++ main.js
@@ -1,1 +1,70 @@
-// number methods 
+// number methods
+// let a = "15"
+// console.log(
+//     typeof(a)
+// );
+// // --a
+// a++
+// console.log(a);
+// console.log(
+//     typeof(a)
+// );
+
+// parsInt()
+// parsFloat()
+
+// let a = "15.554588949"
+// console.log(a);
+// let b = parseInt(a) // nuqtadan keyingi sonini numberga utkazmaydi (15.5 === 15)
+// let y = parseFloat(a) // to'liq stringni numberga utkazadi (15.5 === 15.5)
+// console.log(b);
+// console.log(y);
+
+// Math methods
+
+// let a = 15.5;
+// console.log(a, " default");
+
+// let b = Math.floor(a); // numberni pastga butun qiladi (15.9 === 15)
+// let d = Math.ceil(a); // numberni tepaga butun qiladi (15.1 === 16)
+// let r = Math.round(a) // numberni logichni butun qiladi (15.9 === 16 // 15.4 === 15)
+
+// console.log(b);
+// console.log(d);
+// console.log(r);
+
+// let rnd = Math.random() // randomni raqamlar qaytaradi (0.18916484649)
+
+// let rnd = Math.round(Math.random() * 999)/
+
+// let max = Math.max(8, 84, 6, 28, 32, 28, 5, 29);
+// let min = Math.min(8, 84, 6, 28, 32, -28, 5, 29, 0);
+
+// console.log(max);
+// console.log(min);
+
+// let f = Math.pow(5, 5);
+
+// console.log(f);
+
+
+// ogohlantiruvchilar // alert() / confirm() / prompt()
+
+// alert('salom')
+
+// let a = confirm("yoshing 18 dami ?")
+
+// let a =  prompt('write your name below')
+let age = +prompt('how old are you ?')
+// alert('natija')
+// // let b = parseInt(age)
+// console.log(a);
+console.log(age);
+
+
+// let a = 15
+// let b = 25
+
+// console.log(
+//     a / b
+// );
